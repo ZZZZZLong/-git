b690ec7a479875c61b97f2928bf0c4cecbb3ef56
zs <202105002085@stu.ynufe.edu.cn>
2024-03-11 15:02:39 +0800

    msg

---
+++ Assets/Scripts/UI_1.cs
@@ -0,0 +1,21 @@
+using OpenCover.Framework.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_1 : BasePanel
+{
+    public Button BtnGoUI2;
+
+
+
+    public void OnBtnGoUI2()
+    {
+        print("µã»÷°´Å¥1");
+        UIManager.Instance.OpenPanel(UIConst.UI_2);
+        UIManager newUI = new UIManager();
+    }
+
+
+}
